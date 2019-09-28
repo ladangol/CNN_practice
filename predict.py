29fec6bd8939a73390a714b6ec3dc756044bd954
khanomgol <golshanara.l@gmail.com>
2019-09-29 00:22:26 +0200

    Predicted on test set

--- predict.py
+++ predict.py
@@ -6,8 +6,8 @@ from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 import matplotlib.pyplot as plt
 import csv
-import os
-
+import os, glob
+import numpy as np
 
 categories = ['cat', 'dog']
 
@@ -24,20 +24,20 @@ def load_image(filename, img_size):
 	return img
 
 # load an image and predict the class
-def predict(model_path, testfoler_path):
+def predict(model_path, testfolder_path, img_size):
     model = load_model(model_path)
-    for img in glob.glob(testfoler_path):
-        img = load_image(img)
-	    pred = model.predict(img.reshape([image.reshape(-1, image_size, image_size, 3)]))
+    for img_p in glob.glob(testfolder_path+"*.jpg"):
+        img= load_image(img_p, img_size)
+        pred = model.predict(img.reshape(-1, img_size, img_size, 3))
         print(pred)
         classId = np.argmax(pred)
         className = categories[classId]
-        path, image_name_w_ext = os.path.split(img)
+        path, image_name_w_ext = os.path.split(img_p)
         image_name, image_ext = os.path.splitext(image_name_w_ext)
         print(image_name_w_ext + ': Prediction ' + className)
-        plt.imshow(img, cmap=plt.cm.binary)
-        plt.xlabel(className)
-        plt.show()
+        #plt.imshow(img, cmap=plt.cm.binary)
+        #plt.xlabel(className)
+        #plt.show()
         row = [image_name, classId]
         with open('results.csv', 'a') as csvFile:
             writer = csv.writer(csvFile)
